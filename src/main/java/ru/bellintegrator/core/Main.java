ad0ff3bb2fbe28485c5d05067c0f91d65bfd3d0a
denysord88@aol.com <denysord88@aol.com>
2015-06-05 17:17:00 +0300

    Check push settings

--- src/main/java/ru/bellintegrator/core/Main.java
+++ src/main/java/ru/bellintegrator/core/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String [] args){
         System.out.println("Application started normally! Press any letter and hit Enter to Exit...");
         Scanner in = new Scanner(System.in);
+
         in.next();
     }
 }
