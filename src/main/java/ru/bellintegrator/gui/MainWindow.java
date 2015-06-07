7aca27d1782ca0cdc475c639e8e511bd4aa0d92d
murrzikteam <denysord88@aol.com>
2015-06-07 15:02:32 +0300

    Add utf-8 encoding config to Pom.xml

--- src/main/java/ru/bellintegrator/gui/MainWindow.java
+++ src/main/java/ru/bellintegrator/gui/MainWindow.java
@@ -25,7 +25,7 @@ public class MainWindow extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
-        Button searchButton = new Button("Process...");
+        Button searchButton = new Button("Искать");
         searchButton.setDisable(true);
         Button propertiesButton = new Button("Параметры поиска");
 
